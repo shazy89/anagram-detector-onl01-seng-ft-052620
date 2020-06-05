87cb03f6efab29e1f52acfe133e871ab4a128644
shazy89 <erdo.shazy123@gmail.com>
2020-06-05 08:31:34 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+require "pry"
+
+class Anagram
+attr_accessor :diaper
+
+def initialize (word)
+
+    @diaper = word
+end 
+
+def match(x)
+    
+   x.select {|words| words.split("").sort == @diaper.split("").sort}
+   
+end
+   
+end
+
